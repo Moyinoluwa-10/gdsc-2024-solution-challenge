8421a611a268e72d3c2cd45513881e11d9fc531d
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-02-24 22:35:14 -0600

    feat: added content to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,21 @@ const Home = () => {
     <>
       <section className="hero min-h-screen">
         <Header />
+        <div>
+          <h1>Empowering Healthcare</h1>
+          <p>
+            Providing clean, renewable, and affordable energy solutions for the
+            healthcare sector
+          </p>
+          <a href="">Learn More</a>
+        </div>
+      </section>
+
+      <section>
+        At MediSync, we are dedicated to connecting hospitals with clean and
+        efficient energy sources they need to run daily activities. Our
+        solutions provide access to hydro-powered companies, ensuring access to
+        clean, renewable, and affordable energy for the healthcare sector.
       </section>
     </>
   );
